f10092d50805bab6fa6fd0f221a2ee7d03d60d62
Afif Nahas <afifnahas1998@gmail.com>
2023-07-31 17:35:55 +0300

    added ingestion_to_sql.py file

---
+++ ingestion_to_sql.py
@@ -0,0 +1,42 @@
+import mysql.connector
+import pandas as pd
+
+host = 'localhost'
+user = 'root'
+password = 'root'
+database = 'weather_api'
+
+file = 'weather_forecast.csv'
+
+try:
+    connection = mysql.connector.connect(host=host, user=user, password=password, database=database)
+    if connection.is_connected():
+        print("Connected to the MySQL server.")
+
+        cursor = connection.cursor()
+
+        df = pd.read_csv(file)
+
+        for index, row in df.iterrows():
+            date_time = row['Date and Time']
+            temperature = row['Temperature (°C)']
+            humidity = row['Humidity (%)']
+            weather_description = row['Weather Description']
+            wind_speed = row['Wind Speed (m/s)']
+            wind_direction = row['Wind Direction (°)']
+            clouds = row['Clouds (%)']
+
+            query = "INSERT INTO weather_forecast (dt, temperature, humidity, weather_description, wind_speed, wind_direction, clouds) " \
+                    "VALUES (%s, %s, %s, %s, %s, %s, %s)"
+
+            cursor.execute(query, (date_time, temperature, humidity, weather_description, wind_speed, wind_direction, clouds))
+
+        connection.commit()
+        print("Data inserted successfully.")
+
+        cursor.close()
+        connection.close()
+        print("Connection to the MySQL server is closed.")
+        
+except mysql.connector.Error as e:
+    print("Error connecting to MySQL server:", e)
